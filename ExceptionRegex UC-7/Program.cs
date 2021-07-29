011ad29544d9920adcc86ba0c0445de7516b8dc3
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-29 20:49:14 +0530

    [Guru]Add:Using Exception on Regex Enter Your Password Use Atleast 1 Numeric Character

--- ExceptionRegex UC-7/Program.cs
+++ ExceptionRegex UC-7/Program.cs
@@ -17,7 +17,7 @@ namespace ExceptionRegex_UC_7
                 }
                 else
                 {
-                    Console.WriteLine("Password is Not Valid ");
+                    Console.WriteLine("Password is Not Valid");
                 }
             }
             catch
